639fdd3085f4d214d5092f806839848761cf2bfd
Korshunov Elisey <elisey.korshunov@mail.ru>
2022-11-22 23:10:01 +0300

    added trackbar

---
+++ arrow-OCR/test.py
@@ -0,0 +1,16 @@
+import cv2
+ 
+ 
+def on_change(value):
+    print(value)
+ 
+ 
+img = cv2.imread('img/stef.png')
+ 
+windowName = 'image'
+ 
+cv2.imshow(windowName, img)
+cv2.createTrackbar('slider', windowName, 0, 255, on_change)
+ 
+cv2.waitKey(0)
+cv2.destroyAllWindows()
